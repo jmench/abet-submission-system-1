ffc71536c09e7241984ab6c12869fdc79e9843f0
Shelby Stocker <shelbystocker@att.net>
2019-11-01 16:00:49 -0400

    Create help button.

--- src/main/routes/login.js
+++ src/main/routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mustache = require('../common/mustache')
 var router = express.Router();
+var user = require('../models/User')
 
 /* GET login page */
 router.get('/', function (req, res, next) {
@@ -13,11 +14,19 @@ router.get('/', function (req, res, next) {
 /* POST login page */
 router.post('/', (req, res, next) => {
 	// hard coded username and password
-	if (req.body.username === 'user' && req.body.password === 'password') {
+/*	const user1 = await user.query().where('linkblue_username',req.body.username)
+	if (user1 != null){
+		res.redirect(302, '/course/')
+	}
+	else {
+		res.redirect(302, '/login/')
+	}
+*/	if (req.body.username === 'user' && req.body.password === 'password') {
 		res.redirect(302, '/course/')
 	} else {
 		res.redirect(302, '/login/')
 	}
+
 })
 
 module.exports = router;
